d870ae9af758bdb73272710ae724477cd1a4b323
Richard Max <ricmax.uk@gmail.com>
2023-04-20 17:43:52 +0100

    WIP - Static > WP

--- footer.php
+++ footer.php
@@ -17,80 +17,79 @@
 <?php $theme_folder_path = get_stylesheet_directory_uri(); ?>
 
         <!-- footer begin -->
-        <footer>
-            <div class="container">
-            <div class="row">
-
-            <small>&copy;&nbsp;<?php echo Date('Y'); ?> - <?php bloginfo('name'); ?></small>
-
-<!-- Footer 1 Widget -->
-<div class="col-md-6 col-lg-3">
-  <?php if (is_active_sidebar('footer-1')) : ?>
-    <div>
-      <?php dynamic_sidebar('footer-1'); ?>
-    </div>
-  <?php endif; ?>
-</div>
-
-<!-- Footer 2 Widget -->
-<div class="col-md-6 col-lg-3">
-  <?php if (is_active_sidebar('footer-2')) : ?>
-    <div>
-      <?php dynamic_sidebar('footer-2'); ?>
-    </div>
-  <?php endif; ?>
-</div>
-
-<!-- Footer 3 Widget -->
-<div class="col-md-6 col-lg-3">
-  <?php if (is_active_sidebar('footer-3')) : ?>
-    <div>
-      <?php dynamic_sidebar('footer-3'); ?>
-    </div>
-  <?php endif; ?>
-</div>
-
-<!-- Footer 4 Widget -->
-<div class="col-md-6 col-lg-3">
-  <?php if (is_active_sidebar('footer-4')) : ?>
-    <div>
-      <?php dynamic_sidebar('footer-4'); ?>
-    </div>
-  <?php endif; ?>
-</div>
-<!-- Footer Widgets End -->
-
-</div>
-            </div>
-
-            <div class="subfooter">
-                <div class="container">
-                    <div class="row">
-                      <div class="footer-left col-md-4">
-                      <small>&copy;&nbsp;<?php echo Date('Y'); ?> - <?php bloginfo('name'); ?></small>
-                      </div>
-                      <div class="footer-middle col-md-4">
-
-                      </div>
-                        <div class="footer-right col-md-4 text-right">
-                        
+        <footer id=footer>
+        <div class=container>
+            <div class="row justify-content-between">
+                <div class="col-12 col-lg-3 col-xl-4 mb-5"><a href="/" rel=home class=footer-brand title=Raptive><svg viewbox="0 0 88 23" xmlns=http://www.w3.org/2000/svg><path d="M7.95185 12.5268C10.0605 13.1455 10.5944 13.8556 10.5944 16.0076V18.5266H14.7148V14.633C14.7148 12.5497 13.721 11.3581 11.564 10.8779L8.87304 10.2593C13.1401 10.0992 15.273 8.49603 15.273 5.44973V5.31254C15.273 2.10619 12.8725 0 8.07287 0H0V18.5266H4.12186V11.4052L7.95185 12.5268ZM7.97605 2.84053C10.0847 2.84053 11.1027 3.82503 11.1027 5.54253V5.67971C11.1027 7.39721 10.0847 8.38171 7.97605 8.38171H4.12186V2.83918H7.97605V2.84053ZM61.2613 2.31331V2.22185C61.2613 1.10017 60.4127 0.229986 59.0316 0.229986C57.6506 0.229986 56.8006 1.10017 56.8006 2.22185V2.31331C56.8006 3.43499 57.6491 4.30518 59.0316 4.30518C60.4141 4.30518 61.2613 3.43499 61.2613 2.31331ZM45.8189 7.71731H47.8065V14.1986C47.8065 17.405 48.8487 18.5266 52.3398 18.5266H55.6373V16.0076H53.1158C52.0494 16.0076 51.6849 15.6646 51.6849 14.7487V7.71865H55.5149V5.19957H51.6849V1.07596H47.8065V5.19822H45.8189V7.71731ZM15.589 11.931C15.589 15.7789 18.038 18.7096 21.5533 18.7096C23.7103 18.7096 25.3107 17.6793 26.135 15.9618V18.5266H30.0134V5.19822H26.135V7.76303C25.3107 6.04554 23.7103 5.01531 21.5533 5.01531C18.038 5.01531 15.589 7.94595 15.589 11.7938V11.931ZM46.1834 11.931V11.7938C46.1834 7.94729 43.7345 5.01531 40.2191 5.01531C38.0621 5.01531 36.4618 6.04554 35.6374 7.76303V5.19822H31.759V22.8776H35.6374V15.9618C36.4618 17.6793 38.0621 18.7096 40.2191 18.7096C43.7345 18.7096 46.1834 15.7789 46.1834 11.931ZM88 14.3358H84.5089C84.0234 15.4575 83.0068 16.1447 81.8435 16.1447C80.0012 16.1447 78.8137 14.9773 78.5959 12.6183H88V12.0682C88 7.69444 85.3831 5.01531 81.2854 5.01531C77.4312 5.01531 74.6434 7.92308 74.6434 11.7938V11.931C74.6434 15.9847 77.5038 18.7096 81.6485 18.7096C84.8235 18.7096 87.224 17.1521 87.9986 14.3358M75.3938 5.19822H71.9995L68.897 14.8617L65.7946 5.19822H61.65L66.498 18.5266H70.5458L75.3938 5.19822ZM60.9694 5.19822H57.091V18.5266H60.9694V5.19822ZM81.5517 7.58012C83.0552 7.58012 84.1202 8.58749 84.3878 10.4193H78.7653C79.1042 8.54176 80.1222 7.58012 81.5531 7.58012M19.5159 11.931V11.7938C19.5159 9.32048 20.8741 7.80876 22.886 7.80876C24.8978 7.80876 26.2561 9.32048 26.2561 11.7938V11.931C26.2561 14.4044 24.8978 15.9161 22.886 15.9161C20.8741 15.9161 19.5159 14.4044 19.5159 11.931ZM42.2551 11.931C42.2551 14.4044 40.8969 15.9161 38.885 15.9161C36.8732 15.9161 35.5149 14.4044 35.5149 11.931V11.7938C35.5149 9.32048 36.8732 7.80876 38.885 7.80876C40.8969 7.80876 42.2551 9.32048 42.2551 11.7938V11.931Z"></path></svg></a></div>
+                <div
+                    class="col-12 col-lg-9 col-xl-8">
+                    <div class="row justify-content-between">
+                        <div class="col-6 col-md-3">
+                            <nav>
+                                <ul class="footer-menu footer-menu--sitemap">
+                                    <li class=nav-item><a href=/creators class=nav-link>Creators</a></li>
+                                    <li class=nav-item><a href=/enterprise class=nav-link>Enterprise</a></li>
+                                    <li class=nav-item><a href=/creator-levels class=nav-link>Creator levels</a></li>
+                                </ul>
+                            </nav>
                         </div>
-                      
+                        <div class="col-6 col-md-3">
+                            <nav>
+                                <ul class="footer-menu footer-menu--sitemap">
+                                    <li class=nav-item><a href="https://cafemedia.com/careers/" target=_blank rel="noopener noreferrer" class=nav-link>Careers</a></li>
+                                    <li class=nav-item><a href="https://cafemedia.com/team/" target=_blank rel="noopener noreferrer" class=nav-link>Team</a></li>
+                                    <li class=nav-item><a href="https://cafemedia.com/contact/" target=_blank rel="noopener noreferrer" class=nav-link>Contact us</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="col-6 col-md-3">
+                            <nav>
+                                <ul class="footer-menu footer-menu--sitemap">
+                                    <li class=nav-item><a href="https://adthrive.com/blog/" target=_blank rel="noopener noreferrer" class=nav-link>Blog</a></li>
+                                    <li class=nav-item><a href=/newsroom class=nav-link>Newsroom</a></li>
+                                    <li class=nav-item><a href="https://adthrive.com/resources/" target=_blank rel="noopener noreferrer" class=nav-link>Creator Resources</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                        <div class="col-6 col-md-3">
+                            <nav>
+                                <ul class="footer-menu footer-menu--sitemap">
+                                    <li class=nav-item><a href="https://www.remarkable-voices.com/" target=_blank rel="noopener noreferrer" class=nav-link>Remarkable Voices</a></li>
+                                    <li class=nav-item><a href="https://www.slickstream.com/" target=_blank rel="noopener noreferrer" class=nav-link>Slickstream</a></li>
+                                    <li class=nav-item><a href="https://webwideopen.com/" target=_blank rel="noopener noreferrer" class=nav-link>Web Wide Open</a></li>
+                                </ul>
+                            </nav>
+                        </div>
                     </div>
-                </div>
             </div>
-            <a href="#" id="back-to-top"></a>
-        </footer>
-        <!-- footer close -->
-    </div>
-
-    <!-- Javascript Files
-    ================================================== -->
-  	<script src="<?php echo $theme_folder_path; ?>/js/bootstrap.min.js"></script>
-	
+        </div>
+        <div class="footer-officials row justify-content-between">
+            <div class="col-12 col-md-auto mt-4 order-3 order-md-1"><small class=copyright>&copy; 2023 Raptive.</small></div>
+            <div class="col-6 col-md-auto mt-4 order-2 order-md-2">
+                <nav>
+                    <ul class="footer-menu footer-menu--policies">
+                        <li class=nav-item><a href="https://cafemedia.com/terms/" target=_blank rel="noopener noreferrer" class=nav-link>Terms & Conditions</a></li>
+                        <li class=nav-item><a href="https://cafemedia.com/privacy/" target=_blank rel="noopener noreferrer" class=nav-link>Privacy Policy</a></li>
+                    </ul>
+                </nav>
+            </div>
+            <div class="col-6 col-md-auto mt-4 order-1 order-md-3">
+                <nav>
+                    <ul class="footer-menu footer-menu--socials">
+                        <li class=nav-item><a href="https://www.facebook.com/WeAreRaptive/" title="Raptive on Facebook" target=_blank rel="noopener noreferrer" class=nav-link>Facebook</a></li>
+                        <li class=nav-item><a href="https://www.instagram.com/weareraptive/" title="Raptive on Instagram" target=_blank rel="noopener noreferrer" class=nav-link>Instagram</a></li>
+                        <li class=nav-item><a href="https://www.linkedin.com/company/weareraptive/" title="Raptive on Linkedin" target=_blank rel="noopener noreferrer" class=nav-link>Linkedin</a></li>
+                        <li class=nav-item><a href="https://twitter.com/weareraptive/" title="Raptive on Twitter" target=_blank rel="noopener noreferrer" class=nav-link>Twitter</a></li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
+        </div>
+    </footer>
     
-
-
+    </div>
+  	<!-- script type="text/javascript" src="<?php //echo $theme_folder_path; ?>/js/min/bootstrap.min.js"></script -->
+    <script type="text/javascript" src="<?php echo $theme_folder_path; ?>/js/min/raptive.min.js"></script>
 </body>
 
 </html>
--- index.php
+++ index.php
@@ -15,13 +15,183 @@
 
 get_header();
 ?>
-
-<!-- content begin -->
-<div id="content" class="no-bottom no-top">
-<div class="container">
-
-<?php the_content(); ?>
-
-</div>
-
+	
+<main id=main>
+        <div class=page-content>
+        <?php //the_content(); ?>
+            <section class="hero hero--home">
+                <div class=container>
+                    <div class="row justify-content-between">
+                        <div class="col-12 d-block d-lg-none">
+                            <div class=hero__content>
+                                <h1 class="text-center m-0"><em>Introducing</em> Raptive</h1>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-6 order-2 order-lg-1">
+                            <div class=hero__content>
+                                <h1 class="d-none d-lg-block"><em>Introducing</em> Raptive</h1>
+                                <div>
+                                    <p>CafeMedia and AdThrive are now Raptive, a new kind of company transforming creators into global brands and lasting businesses. Since the very beginning of blogging, we've been allies and advocates for all creators,
+                                        with a seat at every table in the industry. Creators are the future of media. And together, the future is ours to create.</p>
+                                    <div class="btn-wrapper justify-content-center justify-content-lg-start"><a href="https://publisher.adthrive.com/apply/site-info/" class="btn btn-primary" target=_blank rel="noopener noreferrer">Apply now</a></div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-6 order-1 order-lg-2">
+                            <div class=hero-carousel-wrapper>
+                                <div id=lottie style="--fill: var(--raptive-blue)"></div>
+                                <div class=hero-carousel id=heroCarousel data-hero-carousel>
+                                    <div class=hero-carousel__slide data-color=var(--raptive-blue)><img src=/img/home/carousel/style.jpg alt="Style by Emily Henderson" class=hero-carousel__image></div>
+                                    <div class=hero-carousel__slide data-color=var(--raptive-orange)><img src=/img/home/carousel/epic-gardening.jpg alt="Epic Gardening" class=hero-carousel__image></div>
+                                </div>
+                                <div class=hero-carousel-nav id=heroCarouselNav data-hero-carousel-nav>
+                                    <div class=hero-carousel-nav__slide>
+                                        <div class=hero-carousel-nav__selector><span>Style by Emily Henderson</span></div>
+                                    </div>
+                                    <div class=hero-carousel-nav__slide>
+                                        <div class=hero-carousel-nav__selector><span>Epic Gardening</span></div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="boasts raptive-block" data-section-anim>
+                <div class="container container--narrow" data-anim>
+                    <h2 class="h4 text-center"><em>Powering</em> creator independence</h2>
+                </div>
+                <div class=boasts__carousel data-boasts-carousel data-anim>
+                    <div class=boasts__slide>
+                        <div class=boast>
+                            <h3 class=boast__heading>$2b+</h3>
+                            <p class=boast__text>paid directly to creators</p>
+                        </div>
+                    </div>
+                    <div class=boasts__slide>
+                        <div class=boast>
+                            <h3 class=boast__heading>195m</h3>
+                            <p class=boast__text>consumers reached monthly through our creators' websites*</p>
+                        </div>
+                    </div>
+                    <div class=boasts__slide>
+                        <div class=boast>
+                            <h3 class=boast__heading>Top 7</h3>
+                            <p class=boast__text>ranked media company*</p>
+                        </div>
+                    </div>
+                    <div class=boasts__slide>
+                        <div class=boast>
+                            <h3 class=boast__heading>500m</h3>
+                            <p class=boast__text>consumers reached monthly through our creators' social media</p>
+                        </div>
+                    </div>
+                </div>
+                <div class="container container--narrow" data-anim>
+                    <p class="boasts__cite text-center mt-4 mb-0">*Comscore</p>
+                </div>
+            </section>
+            <section class="audiences raptive-block">
+                <div class="container container--narrow">
+                    <div class="audience row justify-content-center align-items-center" data-section-anim>
+                        <div class="col-12 col-lg-6 mb-4 mb-lg-0 pb-3 pb-lg-0" data-anim><img src=/img/home/raptive-for-creators@sm.png data-src=/img/home/raptive-for-creators.png alt="Raptive for Creators" class="audience__image lazyload blur-up"></div>
+                        <div class="col-12 col-lg-6">
+                            <div class=audience__content data-anim>
+                                <h2 class=text-eyebrow>Creators</h2>
+                                <h3>Raptive for Creators</h3>
+                                <p>We're your strategic partner, working hand-in-hand, equipping you with expert advice, smarter strategies, ad management, tailored technology, and inspired opportunities across every part of your business.</p>
+                                <div class="btn-wrapper btn-wrapper--center"><a href=/creators class="btn btn-outline-dark">Learn more</a></div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="audience row justify-content-center align-items-center" data-section-anim>
+                        <div class="col-12 col-lg-6 mb-4 mb-lg-0 pb-3 pb-lg-0 order-lg-2" data-anim><img src=/img/home/raptive-for-enterprise@sm.png data-src=/img/home/raptive-for-enterprise.png alt="Raptive for Enterprise" class="audience__image lazyload blur-up"></div>
+                        <div class="col-12 col-lg-6">
+                            <div class=audience__content data-anim>
+                                <h2 class=text-eyebrow>Enterprise</h2>
+                                <h3>Raptive for Enterprise</h3>
+                                <p>Whether you're currently managing your advertising business in-house or with a managed service, we can help you drive more revenue, cut costs and eliminate inefficiencies. Keep growing today and into the future with our
+                                    unmatched expertise, new revenue streams and world-class tech.</p>
+                                <div class="btn-wrapper btn-wrapper--center"><a href=/enterprise class="btn btn-outline-dark">Learn more</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="solutions raptive-block">
+                <div class="container container--narrow" data-section-anim>
+                    <h2 class=text-center data-anim>Solutions to <em>maximize</em> your ambitions</h2>
+                </div>
+                <div class="container container--narrow">
+                    <div class="row justify-content-center">
+                        <div class="col-12 col-lg-4 mt-3 mt-lg-4" data-section-anim>
+                            <div class=solution data-anim><img src=/img/icon-audience.svg loading=lazy alt="Audience icon" class=solution__image>
+                                <h3 class="h4 m-0">Audience</h3>
+                                <p class=m-0>Captivate bigger audiences. Keep them coming back for more. Share your content.</p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-4 mt-3 mt-lg-4" data-section-anim>
+                            <div class=solution data-anim><img src=/img/icon-revenue.svg loading=lazy alt="Revenue icon" class=solution__image>
+                                <h3 class="h4 m-0">Revenue</h3>
+                                <p class=m-0>Cash in on your passion. Make more with the world's best ad management. Explore new revenue streams.</p>
+                            </div>
+                        </div>
+                        <div class="col-12 col-lg-4 mt-3 mt-lg-4" data-section-anim>
+                            <div class=solution data-anim><img src=/img/icon-business.svg loading=lazy alt="Business icon" class=solution__image>
+                                <h3 class="h4 m-0">Business</h3>
+                                <p class=m-0>Strengthen your strategies. Make moves. Get expert guidance and support to keep your team growing and business thriving.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="testimonials raptive-block">
+                <div class="container container--narrow" data-section-anim>
+                    <h2 class=text-center data-anim><em>Create</em> one-of-a-kind content? You need a one-of-a-kind partner.</h2>
+                </div>
+                <div class="pt-3 pt-md-4 pt-lg-5" data-section-anim>
+                    <div class=testimonials__carousel data-testimonials-carousel data-anim>
+                        <div class=testimonials__slide>
+                            <blockquote class=testimonial><img src=/img/home/testimonials/caz-makepeace.jpg class=testimonial__image alt="Caz Makepeace">
+                                <div class=testimonial__body>
+                                    <p class=h6>&ldquo;They understand creators, anticipate my needs and let me hand off the technology side of monetizing my blog.&rdquo;</p><cite><span>Caz Makepeace</span> <span><a href="https://www.ytravelblog.com/" target=_blank rel="noopener noreferrer">Y Travel Blog</a></span></cite></div>
+                            </blockquote>
+                        </div>
+                        <div class=testimonials__slide>
+                            <blockquote class=testimonial><img src=/img/creators/testimonials/maria-lichty.jpg class=testimonial__image alt="Maria Lichty">
+                                <div class=testimonial__body>
+                                    <p class=h6>&ldquo;The best — trustworthy, knowledgeable, supportive - they provide the best RPMs, great resources and they CARE.&rdquo;</p><cite><span>Maria Lichty</span> <span><a href="https://www.twopeasandtheirpod.com/" target=_blank rel="noopener noreferrer">Two Peas & Their Pod</a></span></cite></div>
+                            </blockquote>
+                        </div>
+                        <div class=testimonials__slide>
+                            <blockquote class=testimonial><img src=/img/home/testimonials/michael-ohalloran.jpg class=testimonial__image alt="Michael O'Halloran">
+                                <div class=testimonial__body>
+                                    <p class=h6>&ldquo;In addition to increasing my ad revenue by over 300%, They freed up my time so I could spend it preparing content.&rdquo;</p><cite><span>Michael O'Halloran</span> <span><a href="https://www.sportsfeelgoodstories.com/" target=_blank rel="noopener noreferrer">Sports Feel Good Stories</a></span></cite></div>
+                            </blockquote>
+                        </div>
+                        <div class=testimonials__slide>
+                            <blockquote class=testimonial><img src=/img/home/testimonials/arnold-kim.jpg class=testimonial__image alt="Arnold Kim">
+                                <div class=testimonial__body>
+                                    <p class=h6>&ldquo;Raptive has helped us optimize our ad revenue while allowing us to maintain our site experience.&rdquo;</p><cite><span>Arnold Kim</span> <span><a href="https://www.macrumors.com/" target=_blank rel="noopener noreferrer">MacRumors</a></span></cite></div>
+                            </blockquote>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <section class="callouts raptive-block">
+                <div class="container container--narrow" data-section-anim>
+                    <div class="callout row justify-content-center align-items-center">
+                        <div class="col-12 col-lg-6 mb-4 mb-lg-0 pb-3 pb-lg-0" data-anim><img src=/img/home/home-cta-callout@sm.png data-src=/img/home/home-cta-callout.png alt="Photo of people" class="callout__image callout__image--centered lazyload blur-up"></div>
+                        <div class="col-12 col-lg-6">
+                            <div class="callout__content callout__content--centered" data-anim>
+                                <h3>Ready to make moves?</h3>
+                                <p>We're here to give you the support, freedom, aha's, and big ideas you need to get to next.</p>
+                                <div class="btn-wrapper btn-wrapper--center"><a href="https://publisher.adthrive.com/apply/site-info/" class="btn btn-primary" target=_blank rel="noopener noreferrer">Apply now</a></div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
 <?php get_footer(); ?>
